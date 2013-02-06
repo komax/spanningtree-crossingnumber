minor fixes and new asserts for numerical stability

src/highdimgraph.py
@@ -287,15 +287,13 @@ class HighDimLine:
         return hash(self.__key())
         
     def __call__(self,x):
-        print x.shape
         paddedX = np.hstack([x, np.ones((x.shape[0], 1), dtype=x.dtype)])
-        #paddedX = np.hstack(x, np.ones(len(x)))
         print paddedX
-        print paddedX.shape
-        print self.theta.shape
         y = np.dot(paddedX, self.theta)
-        print "y=%s" % y
-        return y
+        #print y.shape
+        #print np.allclose(y[0], np.array([1.]))
+        #print "y=%s" % y
+        return y.flatten()
 
     def __str__(self):
        return 'HighDimLine(theta=\n%s, points=\n%s)' % (self.theta,self.X)
@@ -311,7 +309,7 @@ class HighDimLine:
     def is_on(self, p):
         (x,y) = self.__partition(p)
         y_line = self(x)
-        return np.allclose(yline, y)
+        return np.allclose(y_line, y)
 
     def is_above(self, p):
         (x,y) = self.__partition(p)

src/unittests.py
@@ -14,12 +14,17 @@ import numpy as np
 class CrossingTestCase(unittest.TestCase):
     def test_has_crossing(self):
         line = HighDimLine(np.array([(3.,2), (5.,0)])) # y = -x + 5
-        print line
+        #print line
         line_segment = HighDimLineSegment(np.array([(3,0.5), (5,1.5)])) # y = 0.5 x - 1
-        print line_segment
-        points = np.array([(4., 1.), (5., 0.)])
-        np.testing.assert_almost_equal(points[..., -1], line(points[..., :-1]))
-        np.testing.assert_almost_equal(points[..., -1], line_segment(points[..., :-1]))
+        #print line_segment
+        # line_points[0] = segpoints[0] = intersection point
+        line_points = np.array([(4., 1.), (5., 0.)])
+        self.assertTrue(line.is_on(np.array([line_points[0]])))
+        seg_points = line_points
+        seg_points[1,1] = 1.5
+        print line(np.array([[5.]]))
+        np.testing.assert_allclose(line_points[..., -1], line(line_points[..., :-1]))
+        np.testing.assert_allclose(seg_points[..., -1], line_segment(seg_points[..., :-1]))
         self.assertTrue(has_crossing(line, line_segment))
 
     def test_has_no_crossing_linesegment_too_short(self):