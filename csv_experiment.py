96949b013fea0290781fba6ab505605647a8b766
Maximilian Konzack <m.konzack@gmx.de>
2013-03-08 14:57:43 +0100

    forgot to add a trailing space at the end of the each os.system call

--- csv_experiment.py
+++ csv_experiment.py
@@ -3,5 +3,5 @@ import os
 import sys
 
 args = sys.argv[1:]
-os.system('python -m spanningtree.csv_experiment_statistics'+
+os.system('python -m spanningtree.csv_experiment_statistics '+
         ' '.join(args))
--- run_tests.py
+++ run_tests.py
@@ -3,5 +3,5 @@ import os
 import sys
 
 args = sys.argv[1:]
-os.system('python -m spanningtree.test.unittests'+
+os.system('python -m spanningtree.test.unittests '+
         ' '.join(args))
