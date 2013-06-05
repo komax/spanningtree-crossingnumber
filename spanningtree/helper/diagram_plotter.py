f55494bfd71967bf544fcf0c9c2a267a33091928
Maximilian Konzack <m.konzack@gmx.de>
2013-06-05 14:21:51 +0200

    first plots on partial data

--- spanningtree/helper/diagram_plotter.py
+++ spanningtree/helper/diagram_plotter.py
@@ -4,32 +4,44 @@ generates a diagram to compare some results (from csv) in a 2D chart
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-csv_files = [
+algorithm_names = [
+        'mult weights',
+        'fekete lp',
+        'har-peled lp',
+        'connected comp lp'
         ]
 
 markers = [
-        'r--', # red dashed line
-        'bs',  # blue squares
-        'g^',  # green triangles
-        'yo'   # yellow circles
+        'r-', # red line
+        'b-',  # blue line
+        'g-',  # green line
+        'y-'   # yellow line
         ]
 
-def plot_chart(filename):
+def plot_chart(filename, csv_files):
     plt.figure()
     filename = filename + '.png'
     # TODO add legend
     for (i, csv_file) in enumerate(csv_files):
         data = np.genfromtxt(csv_file, delimiter=',', skip_header=1)
         my_marker = markers[i]
-        plt.plot(data[:, 0], data[:, 4], my_marker)
+        #plt.plot(data[:, 0], data[:, 4], my_marker)
+        plt.plot(data[:, 0], data[:, 5], my_marker)
+        #plt.plot(data[:, 0], data[:, 6], my_marker)
+    points = np.arange(2, 101)
+    plt.plot(points, np.sqrt(points), 'k--')
     plt.xlabel('number of points')
-    plt.ylabel('crossing number')
+    plt.ylabel('average crossing number')
     plt.savefig(filename)
     plt.show()
 
 def main():
-    pass
+    assert len(sys.argv) >= 3
+    filename = sys.argv[1]
+    csv_files = sys.argv[2:]
+    plot_chart(filename, csv_files)
 
 if __name__ == '__main__':
     main()
