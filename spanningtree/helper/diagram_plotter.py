47e48a3733a3a91875bfa0f590af8fedafdefd40
Maximilian Konzack <m.konzack@gmx.de>
2013-06-12 00:23:52 +0200

    updated diagram plotter with some reference functions

--- spanningtree/helper/diagram_plotter.py
+++ spanningtree/helper/diagram_plotter.py
@@ -7,11 +7,14 @@ import pylab
 import numpy as np
 import sys
 
-algorithm_names = [
-        'mult weights',
-        'fekete lp',
-        'har-peled lp',
-        'connected comp lp'
+legend_names = [
+#        'mult weights',
+#        'fekete lp',
+        'har-peled lp (5 runs)',
+        'har-peled lp (10 runs)',
+        'har-peled lp (15 runs)',
+        'har-peled lp (20 runs)'
+#        'connected comp lp'
         ]
 
 markers = [
@@ -24,20 +27,24 @@ markers = [
 def plot_chart(filename, csv_files):
     plt.figure()
     filename = filename + '.png'
-    # TODO add legend
     for (i, csv_file) in enumerate(csv_files):
         data = np.genfromtxt(csv_file, delimiter=',', skip_header=1)
         my_marker = markers[i]
-        my_label = algorithm_names[i]
-        plt.plot(data[:, 0], data[:, 4], my_marker, label=my_label)
-        #plt.plot(data[:, 0], data[:, 2], my_marker, label=my_label)
-        #plt.plot(data[:, 0], data[:, 6], my_marker)
+        my_label = legend_names[i]
+        # x: n y: crossing number
+        #plt.plot(data[:, 0], data[:, 4], my_marker, label=my_label)
+        # x: n y: cpu time
+        plt.plot(data[:, 0], data[:, 2], my_marker, label=my_label)
+        # x: n y: average crossing number
+        #plt.plot(data[:, 0], data[:, 5], my_marker, label=my_label)
     points = np.arange(2, 101)
-    #plt.plot(points, 0.5*points**2, 'k--')
-    plt.plot(points, np.sqrt(points), 'k--', label='$\sqrt{n}$')
+    #plt.plot(points, 0.5*points*2, 'k--', label='$f(x)=0.5x^2$')
+    plt.plot(points, points*3, 'k--', label='$f(x)=3x$')
+    #plt.plot(points, np.sqrt(points), 'k--', label='$f(x)=\sqrt{x}$')
+    #plt.plot(points, np.log(points), 'k--', label='$f(x)=\log(x)$')
     plt.xlabel('number of points')
-    #plt.ylabel('CPU time (in sec)')
-    plt.ylabel('crossing number')
+    plt.ylabel('CPU time (in s)')
+    #plt.ylabel('average crossing number')
     pylab.legend(loc=0)
     plt.savefig(filename)
     plt.show()
