9c4b511c91e8a7b62bc106fc8c3f01478e75f6b5
Maximilian Konzack <m.konzack@gmx.de>
2013-12-17 15:00:11 +0100

    Added plotting of results for fekete

--- spanningtree/helper/diagram_plotter.py
+++ spanningtree/helper/diagram_plotter.py
@@ -9,18 +9,15 @@ import sys
 
 legend_names = [
 #        'mult weights',
-#        'fekete lp',
-        'har-peled lp (5 runs)',
-        'har-peled lp (10 runs)',
-        'har-peled lp (15 runs)',
-        'har-peled lp (20 runs)'
-#        'connected comp lp'
+        'fekete lp',
+#        'har-peled lp (20 runs)'
+        'connected comp lp'
         ]
 
 markers = [
-        'r-', # red line
+#        'r-', # red line
         'b-',  # blue line
-        'g-',  # green line
+#        'g-',  # green line
         'y-'   # yellow line
         ]
 
@@ -34,17 +31,21 @@ def plot_chart(filename, csv_files):
         # x: n y: crossing number
         #plt.plot(data[:, 0], data[:, 4], my_marker, label=my_label)
         # x: n y: cpu time
-        plt.plot(data[:, 0], data[:, 2], my_marker, label=my_label)
+        #plt.plot(data[:, 0], data[:, 2], my_marker, label=my_label)
         # x: n y: average crossing number
-        #plt.plot(data[:, 0], data[:, 5], my_marker, label=my_label)
+        plt.plot(data[:, 0], data[:, 5], my_marker, label=my_label)
+        # for fekete trails
+        # x: n y: failed trails
+        #plt.plot(data[:, 0], data[:, -1], my_marker, label=my_label)
     points = np.arange(2, 101)
-    #plt.plot(points, 0.5*points*2, 'k--', label='$f(x)=0.5x^2$')
-    plt.plot(points, points*3, 'k--', label='$f(x)=3x$')
-    #plt.plot(points, np.sqrt(points), 'k--', label='$f(x)=\sqrt{x}$')
+    #plt.plot(points, 4*points, 'k--', label='$f(x)=4x$')
+    #plt.plot(points, points*3, 'k--', label='$f(x)=3x$')
+    plt.plot(points, np.sqrt(points), 'k--', label='$f(x)=\sqrt{x}$')
     #plt.plot(points, np.log(points), 'k--', label='$f(x)=\log(x)$')
     plt.xlabel('number of points')
-    plt.ylabel('CPU time (in s)')
-    #plt.ylabel('average crossing number')
+    #plt.ylabel('average trails adding a heavy weight edge within a cc')
+    plt.ylabel('average crossing number')
+    #plt.ylabel('crossing number')
     pylab.legend(loc=0)
     plt.savefig(filename)
     plt.show()
